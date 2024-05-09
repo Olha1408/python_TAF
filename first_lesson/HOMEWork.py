ccb0dcec519b234dc49bbf44a04e7a9f0a12f8a0
Olya <olhavasylivnaqa@gmail.com>
2024-05-09 18:30:27 +0300

    first commit

---
+++ first_lesson/HOMEWork.py
@@ -0,0 +1,25 @@
+# feet = 10
+# inches =5
+
+# feet = 30.48
+# inch = 2.54
+#
+# print("Enter your values for Inches:")
+# s  = float(input())
+# print("Enter your values for Feet:")
+# f= float(input())
+#
+# print(s*inch , f*feet )
+
+
+#######################
+#a=10
+# for k in range (1, 7):
+#     a=a + k
+#     print(a)
+
+
+list_of_values=[10,5,12,25,13,6,4,22,106,94,46]
+for k in list_of_values:
+    if k/4 >5:
+        print(k)
---
+++ first_lesson/demo.py
@@ -0,0 +1,13 @@
+value=0
+# while value < 4:
+#     print(value)
+#     value +=1
+
+
+# my_list = [1,2,3,4,5]
+# for list_value in my_list:
+#     print(list_value)
+#
+
+for value in range(0,4):
+    print(value)
